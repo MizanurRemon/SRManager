b4c97ebb3da79f0e890e224041ce99a6fe92bc1d
MizanurRemon <remonmizanur@gmail.com>
2023-11-30 09:55:05 +0600

    SRM

--- features/report/report_presentation/src/main/java/com/srmanager/report_presentation/report/ReportScreen.kt
+++ features/report/report_presentation/src/main/java/com/srmanager/report_presentation/report/ReportScreen.kt
@@ -4,13 +4,7 @@ import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.FabPosition
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -18,10 +12,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
-import com.srmanager.core.designsystem.R
 import com.srmanager.core.designsystem.components.AppToolbarCompose
-import com.srmanager.core.designsystem.r
-import com.srmanager.core.designsystem.theme.APP_DEFAULT_COLOR
 import com.srmanager.core.designsystem.theme.bodyBoldTextStyle
 import com.srmanager.core.designsystem.R as DesignSystemR
 import com.srmanager.core.common.R as CommonR
