f898d789004529f84e9f34afedbf60c8e75e84d4
remon <remon@impelitsolutions.com>
2024-06-22 14:18:13 +0600

    new

--- features/order/order_presentation/src/main/java/com/srmanager/order_presentation/order/OrderViewModel.kt
+++ features/order/order_presentation/src/main/java/com/srmanager/order_presentation/order/OrderViewModel.kt
@@ -9,7 +9,6 @@ import androidx.lifecycle.viewModelScope
 import com.srmanager.core.common.util.UiEvent
 import com.srmanager.core.common.util.UiText
 import com.srmanager.core.designsystem.generatePDF
-import com.srmanager.core.designsystem.generatePDF2
 import com.srmanager.order_domain.use_case.OrderUseCases
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.DelicateCoroutinesApi
