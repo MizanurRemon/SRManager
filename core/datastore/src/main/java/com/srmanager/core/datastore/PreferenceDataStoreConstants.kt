34b68d23b6088d0274d509ef5a9017f9003a0075
Mizanur Remon <remonmizanur@gmail.com>
2026-05-29 12:45:56 +0600

    up

--- core/datastore/src/main/java/com/srmanager/core/datastore/PreferenceDataStoreConstants.kt
+++ core/datastore/src/main/java/com/srmanager/core/datastore/PreferenceDataStoreConstants.kt
@@ -7,11 +7,7 @@ import androidx.datastore.preferences.core.stringPreferencesKey
 
 object PreferenceDataStoreConstants {
     val IS_LOGGED_IN = booleanPreferencesKey("is_logged_in")
-    val IS_PROFILE_COMPLETE = booleanPreferencesKey("is_profile_complete")
     val COMPANY_ID = intPreferencesKey("company_id")
     val ACCESS_TOKEN = stringPreferencesKey("access_token")
-    val COMPANY_ID = intPreferencesKey("company_id")
-    val LANGUAGE_TAG = stringPreferencesKey("language_tag")
     val REFRESH_TOKEN = stringPreferencesKey("refresh_token")
-    val IS_Notification_Enable = booleanPreferencesKey("is_notification_enable")
 }
